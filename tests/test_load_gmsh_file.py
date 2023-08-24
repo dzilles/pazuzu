41519b45e809b4d922ba624a8c18378fcee6edfa
dzilles <zilles.dz@gmail.com>
2023-08-24 21:53:57 +0200

    Added system test data

--- tests/test_load_gmsh_file.py
+++ tests/test_load_gmsh_file.py
@@ -12,23 +12,23 @@ def test_load_2D_gmsh_coordinates():
     msh = mesh.Mesh("vertex_2D.msh");
 
     if (build_test_cases):
-        numpy.save("data/test_load_gmsh_coordinates", msh.coordinates)
-        numpy.save("data/test_load_gmsh_cell_node_ids", msh.cell_node_ids)
-        numpy.save("data/test_load_gmsh_face_node_ids", msh.face_node_ids)
-        numpy.save("data/test_load_gmsh_neighbor_ids", msh.neighbor_ids)
-        numpy.save("data/test_load_gmsh_adjacent_face_ids", msh.adjacent_face_ids)
+        numpy.save("data/system_tests/test_load_gmsh_coordinates", msh.coordinates)
+        numpy.save("data/system_tests/test_load_gmsh_cell_node_ids", msh.cell_node_ids)
+        numpy.save("data/system_tests/test_load_gmsh_face_node_ids", msh.face_node_ids)
+        numpy.save("data/system_tests/test_load_gmsh_neighbor_ids", msh.neighbor_ids)
+        numpy.save("data/system_tests/test_load_gmsh_adjacent_face_ids", msh.adjacent_face_ids)
 
-    test_data = numpy.load("data/test_load_gmsh_coordinates.npy")
+    test_data = numpy.load("data/system_tests/test_load_gmsh_coordinates.npy")
     assert (numpy.array_equal(msh.coordinates, test_data))
 
-    test_data = numpy.load("data/test_load_gmsh_cell_node_ids.npy")
+    test_data = numpy.load("data/system_tests/test_load_gmsh_cell_node_ids.npy")
     assert (numpy.array_equal(msh.cell_node_ids, test_data))
 
-    test_data = numpy.load("data/test_load_gmsh_face_node_ids.npy")
+    test_data = numpy.load("data/system_tests/test_load_gmsh_face_node_ids.npy")
     assert (numpy.array_equal(msh.face_node_ids, test_data))
 
-    test_data = numpy.load("data/test_load_gmsh_neighbor_ids.npy")
+    test_data = numpy.load("data/system_tests/test_load_gmsh_neighbor_ids.npy")
     assert (numpy.array_equal(msh.neighbor_ids, test_data))
 
-    test_data = numpy.load("data/test_load_gmsh_adjacent_face_ids.npy")
+    test_data = numpy.load("data/system_tests/test_load_gmsh_adjacent_face_ids.npy")
     assert (numpy.array_equal(msh.adjacent_face_ids, test_data))
